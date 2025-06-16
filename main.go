5f4a48f9db92377901b59e15395ea95564aa79a0
Aaron McCready <aaron.mccready@cgi.com>
2025-06-16 15:21:41 +0100

    Naming conventions on variables

--- main.go
+++ main.go
@@ -20,4 +20,13 @@ func main() {
 		log.Fatal(err)
 	}
 
+	//var name type   -- Note that  if a variable function or type starts with a CAPITAL letter it is considered EXPORTED, and can be accessed outside the package
+	// camelCase is used
+
+	var vName string = "Aaron"
+	var v1, v2 = 1.2, 3.4
+	var v3 = "hello" //figures out the type for us
+	v4 := 2.4        // shortcut when initially assigning value
+	v4 = 5.4
+
 }
