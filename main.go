60a9c21238795d2af622b06aa8f3ff2023114908
Aaron McCready <aaron.mccready@cgi.com>
2025-06-17 23:51:48 +0100

    Slices

--- main.go
+++ main.go
@@ -150,4 +150,41 @@ func main() {
 	bStr := string(byteArr[:])
 	pl("I'm a string :", bStr)
 
+	// slices
+	// var name []datatype
+
+	sl1 := make([]string, 6)
+
+	sl1[0] = "Society"
+	sl1[1] = "of"
+	sl1[2] = "the"
+	sl1[3] = "Simulated"
+	sl1[4] = "Universe"
+	pl("Slice size :", len(sl1))
+	for i := 0; i < len(sl1); i++ {
+		pl("Simple loop i++ :", sl1[i])
+	}
+
+	for _, x := range sl1 {
+		pl("range loop :", x)
+	}
+
+	// Slice
+	sArr := [5]int{1, 2, 3, 4, 5}
+	sl3 := sArr[0:2] //first up to the 2nd value
+	pl("1st 3 :", sArr[:3])
+	pl("Last 3 :", sArr[2:])
+	sArr[0] = 10
+	pl("s13 :", sl3)
+	sl3[0] = 1
+	pl("sArr :", sArr)
+
+	sl3 = append(sl3, 12)
+	pl("sl3 :", sl3)
+	pl("sArr :", sArr)
+
+	sl4 := make([]string, 6)
+	pl("sl4 :", sl4)
+	pl("sl4[0] :", sl4[0])
+
 }
