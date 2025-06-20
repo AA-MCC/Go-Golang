d8d9f57710fa6d8cff0e376905ff0cdef0123aed
Aaron McCready <aaron.mccready@cgi.com>
2025-06-20 16:11:52 +0100

    Added update and delete functionality

--- README.md
+++ README.md
@@ -10,4 +10,13 @@ go run todo-app\cmd\main.go -action add -desc "Buy bread"
 To list all tasks:-
 go run todo-app\cmd\main.go -action list
 
+To update a task:-
+Use the "log/slog" structured logging package to log errors and when data is saved to disk
+- Use the "context" package to add a TraceID to enable traceability of calls through the solution by adding it to all logs
+
+To delete a task:-
+ go run todo-app\cmd\main.go -action delete -id  "72cc896b-ef77-4f06-987a-094fd3e47e58"
+
+
+
 
--- todo-app/cmd/main.go
+++ todo-app/cmd/main.go
@@ -9,7 +9,6 @@ import (
 	"github.com/google/uuid"
 )
 
-var pl = fmt.Println
 var fileName = "todolist.json"
 var todos []TodoTask
 
@@ -20,25 +19,23 @@ type TodoTask struct {
 }
 
 func main() {
-	pl("Hello Go.. \n")
+	fmt.Println("Hello Go.. ")
 
 	loadTodos()
-	// listTodos()
 
 	action := flag.String("action", "add", "Action : add, list, update, delete")
-
 	desc := flag.String("desc", "", "Description for add/update")
-
-	// listItems := flag.String("list", "", "List all todo tasks")
-	// updateItem := flag.String("update", "", "Update todo task")
-	// deleteItem := flag.String("delete", "", "Delete todo task")
-	// status := flag.String("status", "not started", "Todo task status: not started/started/completed")
+	status := flag.String("status", "not started", "Todo task status: not started/started/completed")
+	id := flag.String("id", "", "Todo task ID for update/delete")
 
 	flag.Parse()
-	pl("description is :", *desc)
-	pl("action is :", *action)
 
-	// handle the commands - investigate options
+	fmt.Println("id is :", *id)
+	fmt.Println("action is :", *action)
+	fmt.Println("description is :", *desc)
+	fmt.Println("Status is :", *status)
+
+	// handle the commands
 
 	switch *action {
 	case "add":
@@ -47,24 +44,28 @@ func main() {
 			saveTodos()
 			listTodos()
 		}
-
 	case "list":
 		listTodos()
+	case "update":
+		fmt.Println(*id, *desc, *status)
+		updateTodo(*id, *desc, *status)
+		saveTodos()
+	case "delete":
+		deleteTodo(*id)
+		saveTodos()
 	default:
-		pl("Invalid action.")
-
+		fmt.Println("Invalid action.")
 	}
 
 }
 
-// Saves the current todos slice to file
 func saveTodos() {
 	data, err := json.MarshalIndent(todos, "", "  ")
 	if err != nil {
-		pl("Error saving todos:", err)
+		fmt.Println("Error saving todos:", err)
 		return
 	}
-	pl("Saved Todo")
+	fmt.Println("Saved Todo")
 	_ = os.WriteFile(fileName, data, 0644)
 }
 
@@ -87,29 +88,65 @@ func loadTodos() {
 		_ = json.Unmarshal(data, &todos)
 
 	}
-	// pl("load to do data: ", data)
-	pl("Loaded Todos :", todos)
+	fmt.Println("Loaded Todos :", todos)
 }
 
 func listTodos() {
-	pl("To-Do List:")
+	fmt.Println("To-Do List:")
 	for _, t := range todos {
-		fmt.Printf("#%d [%s]: %s\n", t.ID, t.Status, t.Description)
+		fmt.Println(t.ID, t.Status, t.Description)
 	}
 }
 
 func addTodo(description string) {
 	if description == "" {
-		pl("Description is required to add a todo")
+		fmt.Println("Description is required to add a todo")
 		return
 	}
 	id := uuid.New().String()
-	// id := getNextID()
+
 	newTodo := TodoTask{
 		ID:          id,
 		Description: description,
 		Status:      "not started",
 	}
 	todos = append(todos, newTodo)
-	pl("Added todo: ", newTodo.ID, newTodo.Description)
+	fmt.Println("Added todo: ", newTodo.ID, newTodo.Description)
+}
+
+func updateTodo(id string, newDescription string, newStatus string) {
+	listTodos()
+	if newDescription == "" && newStatus == "" {
+		fmt.Println("Description or status is needed to update")
+		return
+	}
+
+	// Find and update the todo by ID
+	for i, t := range todos {
+		if t.ID == id {
+			if newDescription != "" {
+				todos[i].Description = newDescription
+			}
+			if newStatus != "" {
+				todos[i].Status = newStatus
+			}
+			fmt.Println("Updated todo:", todos[i].ID, todos[i].Description, todos[i].Status)
+			return
+		}
+	}
+	fmt.Println("Todo with ID", id, "not found")
+}
+
+func deleteTodo(id string) {
+	fmt.Println("todos in delete function:", todos)
+	fmt.Println("id to be deleted is :", id)
+
+	for i, t := range todos {
+		if t.ID == id {
+			todos = append(todos[:i], todos[i+1:]...)
+			fmt.Println("Deleted todo:", t.ID, t.Description, t.Status)
+			return
+		}
+	}
+	fmt.Println("Todo with ID", id, "not found")
 }
